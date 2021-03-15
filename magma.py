3c5e01d519699352d845bbdb41f296bd3fb982ac
WarpWing <professornitro@gmail.com>
2021-03-15 11:40:19 -0400

    Finished Midpoint from x1,y1,x2,y2

--- magma.py
+++ magma.py
@@ -6,15 +6,20 @@ def dash():
 
 
 
-def hypocalc():
-    a = int(input('What is the value of A?: '))
-    b = int(input('What is the value of B?: '))
+def hypocalc(a,b):
     c = a ** 2 + b ** 2
     d = math.sqrt(c) 
-    if type(d) is float:
-        print(c)
+    if type(d) is float: #Float is a number with decimals as opposed to Integer which is a whole number.
+        print(f"{c} does not simply. This is the decimal return. Please make sure to meet roundup requirements.")
     else: 
-        print(d)
+        print(f"{c} does simply and returns {d} as it's square root")
+    #Note: Does not do automatic Radical Conversion. Should add that soon.
+
+def midpoint(x1, x2, y1, y2):
+    xout = (x1 + x2) / 2 
+    yout = (y1 + y2) / 2
+    print(f"The midpoint would be ({xout},{yout})")
+
     
     
 
@@ -22,12 +27,21 @@ def hypocalc():
 dash()
 print('Welcome to Magma. Please press a number on the list given for the corresponding operation')
 print('1. Hypotensue Calculator')
+print('2. Midpoint Calculator from x1,y1 and x2,y2')
 dash()
 x = input('What would you like to do: ')
 
-if x == "1":
-    hypocalc()
 
+if x == "1":
+    a = int(input('What is the value of A?: '))
+    b = int(input('What is the value of B?: '))
+    hypocalc(a,b)
+elif x == "2":
+    x1 = int(input('What is the value of x1?: '))
+    y1 = int(input('What is the value of y1?: '))
+    x2 = int(input('What is the value of x2: '))
+    y2 = int(input('What is the value of y2?: '))
+    midpoint(x1, x2, y1, y2)
 else:
     exit()
 
